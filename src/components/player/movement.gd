037b24b28009839621ada6951370851a65fbed75
Morgan Jones <me@morganlabs.dev>
2024-11-26 23:55:29 +0000

    feat: rising and falling animations for player

--- src/components/player/movement.gd
+++ src/components/player/movement.gd
@@ -9,6 +9,8 @@ const JUMP_TIME_DESCENT := 0.3
 
 var has_double_jumped := false
 
+@onready var sprite = $AnimatedSprite2D
+
 @onready var jump_velocity = ((2.0 * JUMP_HEIGHT) / JUMP_TIME_PEAK) * -1.0
 @onready var jump_gravity = ((-2.0 * JUMP_HEIGHT) / (JUMP_TIME_PEAK * JUMP_TIME_PEAK)) * -1.0
 @onready var fall_gravity = ((-2.0 * JUMP_HEIGHT) / (JUMP_TIME_DESCENT * JUMP_TIME_DESCENT)) * -1.0
@@ -21,9 +23,14 @@ func _get_gravity() -> float:
 func _physics_process(delta: float) -> void:
 	velocity.y += _get_gravity() * delta
 
-	var direction := Input.get_axis("Left", "Right")
-	var sprint_multiplier := SPRINT_MULTIPLIER if Input.is_action_pressed("Sprint") else 1.0
+	handle_jump()
+	handle_movement()
+	handle_animation()
 
+	move_and_slide()
+
+
+func handle_jump():
 	if Input.is_action_pressed("Jump"):
 		if is_on_floor():
 			velocity.y = jump_velocity
@@ -35,10 +42,24 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		has_double_jumped = false
 
+
+func handle_movement():
+	var direction := Input.get_axis("Left", "Right")
+	var sprint_multiplier := SPRINT_MULTIPLIER if Input.is_action_pressed("Sprint") else 1.0
+
 	if direction:
 		velocity.x = direction * SPEED * sprint_multiplier
 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+
+func handle_animation():
+	if !is_on_floor() && velocity.y < 0:
+		sprite.play("rising")
+		return
+	if !is_on_floor() && velocity.y > 0:
+		sprite.play("falling")
+		return
+
+	sprite.play("running")
