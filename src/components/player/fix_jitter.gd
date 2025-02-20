76cedaba66c8d16d72d360feb00f16b7d2a5da9d
Morgan Jones <me@morganlabs.dev>
2025-02-20 22:51:46 +0000

    feat: macOS Export Option

--- src/components/player/fix_jitter.gd
+++ src/components/player/fix_jitter.gd
@@ -11,22 +11,22 @@ var parent_prev_position: Vector2
 var parent_curr_position: Vector2
 
 func _physics_process(_delta):
-    parent_prev_position = parent_curr_position
-    parent_curr_position = parent.global_position
+	parent_prev_position = parent_curr_position
+	parent_curr_position = parent.global_position
 
 func _process(_delta):
-    var factor = Engine.get_physics_interpolation_fraction()
+	var factor = Engine.get_physics_interpolation_fraction()
 
-    self.global_position.x = parent_prev_position.x + (parent_curr_position.x - parent_prev_position.x) * factor
-    self.global_position.y = (parent_prev_position.y + (parent_curr_position.y - parent_prev_position.y) * factor) + 5
+	self.global_position.x = parent_prev_position.x + (parent_curr_position.x - parent_prev_position.x) * factor
+	self.global_position.y = (parent_prev_position.y + (parent_curr_position.y - parent_prev_position.y) * factor) + 5
 
 func _ready():
-    if (!parent):
-        parent = get_parent()
+	if (!parent):
+		parent = get_parent()
 
-    if ("global_position" in parent):
-        parent_prev_position = parent.global_position
-        parent_curr_position = parent.global_position
-    else:
-        set_process(false)
-        set_physics_process(false)
+	if ("global_position" in parent):
+		parent_prev_position = parent.global_position
+		parent_curr_position = parent.global_position
+	else:
+		set_process(false)
+		set_physics_process(false)
