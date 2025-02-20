495cab5242bdbc6272a27aa64629c8bb757e6ea0
Morgan Jones <me@morganlabs.dev>
2025-02-20 01:45:09 +0000

    feat: player manual movement
    To better align with the user requirements, the player can now use Left/Right or A/D keys to move left and right.

--- src/components/player/player.gd
+++ src/components/player/player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-enum DIRECTIONS { RISING, FALLING, FORWARD }
+enum DIRECTIONS_X { STILL, RIGHT, LEFT }
+enum DIRECTIONS_Y { RISING, FALLING, GROUND }
 
+const ACCELERATION := 2500.0
+const FRICTION := 800.0
 const JUMP_HEIGHT := 70.0
 const JUMP_TIME_PEAK := 0.5
 const JUMP_TIME_DESCENT := 0.5
@@ -12,7 +15,8 @@ var mana := 0
 
 var has_jumped := false
 var has_double_jumped := false
-var direction := DIRECTIONS.FORWARD
+var direction_x := DIRECTIONS_X.STILL
+var direction_y := DIRECTIONS_Y.GROUND
 
 @onready var sprite := $AnimatedSprite2D
 
@@ -27,13 +31,30 @@ func _get_gravity() -> float:
 
 func _physics_process(delta: float) -> void:
 	velocity.y += _get_gravity() * delta
-	velocity.x = speed
 
-	get_direction(delta)
+	handle_movement(delta)
+	get_y_direction(delta)
 	handle_jump()
 	handle_animation()
 
 	move_and_slide()
+
+func handle_movement(delta: float):
+	if Input.is_action_pressed("Move Left"):
+		direction_x = DIRECTIONS_X.LEFT
+		sprite.flip_h = true
+	elif Input.is_action_pressed("Move Right"):
+		direction_x = DIRECTIONS_X.RIGHT
+		sprite.flip_h = false
+	else:
+		direction_x = DIRECTIONS_X.STILL
+
+	if direction_x == DIRECTIONS_X.LEFT:
+		velocity.x = move_toward(velocity.x, -1 * speed, ACCELERATION * delta)
+	elif direction_x == DIRECTIONS_X.RIGHT:
+		velocity.x = move_toward(velocity.x, 1 * speed, ACCELERATION * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
 
 func handle_jump():
@@ -46,7 +67,7 @@ func handle_jump():
 	if Input.is_action_just_pressed("Jump") and !is_on_floor() and !has_double_jumped:
 		var multiplier = 0.75 if has_jumped else 1.0
 		velocity.y = jump_velocity * multiplier
-		direction = DIRECTIONS.RISING
+		direction_y = DIRECTIONS_Y.RISING
 		has_double_jumped = true
 		return
 
@@ -56,21 +77,21 @@ func handle_jump():
 
 
 func handle_animation():
-	if (direction == DIRECTIONS.RISING) && (sprite.animation != "rising"):
+	if (direction_y == DIRECTIONS_Y.RISING) && (sprite.animation != "rising"):
 		sprite.play("rising")
-	elif (direction == DIRECTIONS.FALLING) && (sprite.animation != "falling"):
+	elif (direction_y == DIRECTIONS_Y.FALLING) && (sprite.animation != "falling"):
 		sprite.play("falling")
-	elif (is_on_floor()) && (direction == DIRECTIONS.FORWARD) && (sprite.animation != "running"):
+	elif (is_on_floor()) && (direction_y == DIRECTIONS_Y.GROUND) && (sprite.animation != "running"):
 		sprite.play("running")
 
 
-func get_direction(delta: float):
+func get_y_direction(delta: float):
 	if velocity.y < 0:
-		direction = DIRECTIONS.RISING
+		direction_y = DIRECTIONS_Y.RISING
 	elif velocity.y > (fall_gravity * delta):
-		direction = DIRECTIONS.FALLING
+		direction_y = DIRECTIONS_Y.FALLING
 	else:
-		direction = DIRECTIONS.FORWARD
+		direction_y = DIRECTIONS_Y.GROUND
 
 
 func add_mana() -> void:
